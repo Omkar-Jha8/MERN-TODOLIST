ee57ec42fe0151ed40700c664dd0a2868029ca5d
Omkar-jha8 <omforweb75@gmail.com>
2025-08-22 17:59:08 +0530

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,23 +4,35 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors"
+import path from "path"
 
 dotenv.config();
 
 const app = express()
 
-
-app.use(
-    cors({
-        origin: "http://localhost:5173",
-    })
-); // Cross Site Resource Sharing allows you to share your data to the set origin
+if (process.env.NODE_ENV !== "production") {
+    app.use(
+        cors({
+            origin: "http://localhost:5173",
+        })
+    ); // Cross Site Resource Sharing allows you to share your data to the set origin
+}
 app.use(express.json()) //Middleware
 app.use(rateLimiter)  //Middleware
 
 app.use('/api/notes', notesRoute) // Routes Related to Notes
 
 const PORT = process.env.PORT || 5001 // Port Where Server Will Start
+
+const __dirname = path.resolve()
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get(/.*/, (req, res) => {
+        res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
+    });
+}
 
 connectDB().then(() => {
     app.listen(PORT, () => {
